407110c988e298f20461538b46572ea7955c7e83
Miriam Perez <misupere@ucsc.edu>
2021-05-03 16:06:40 -0700

    add note to hit space to go back to title

--- src/scenes/Tutorial.js
+++ src/scenes/Tutorial.js
@@ -29,6 +29,7 @@ class Tutorial extends Phaser.Scene{
         this.add.text(game.config.width/2 - 350, game.config.height/2 + 0 * customHeight, "Press Arrow keys\n to move around", menuConfig).setOrigin(0.5);
         this.add.text(game.config.width/2 + 625, game.config.height/2 + -1 * customHeight, "Throw Potions with\nQ, W, and E\nto destroy obstacles!", menuConfig).setOrigin(0.5);
         this.add.text(game.config.width/2 + 625, game.config.height/2 + 1.5 * customHeight, "WATER Potion destroys FIRE Obstacles\nGRASS Potion destroys WATER Obstacles\nFIRE Potion destroys GRASS Obstacles", menuConfig).setOrigin(0.5);
+        this.add.text(game.config.width/6, game.config.height/2 + 5.5 * customHeight, "Press Space to go to the Menu!", menuConfig).setOrigin(0.5, 0);
 
         // Define Keys
         keySpace = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
